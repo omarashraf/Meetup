e2bf431cb4bc8afc2539ede164acf60b38b163d0
Omar <o.abouzaid93@gmail.com>
2015-11-11 21:04:41 +0200

    All models for milestone 1 have already been generated

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,14 +1,5 @@
 class ProfilesController < ApplicationController
   def index
-  	@user = User.find(3)
-  	@posts_me = Post.where(user_sender_id: 3, user_receiver_id: nil)
-  	@posts_others = Post.where(user_receiver_id: 3)
-  	@friends = Friendship.where(user_request_sender_id: 3, accept: true)
-  	@friends2 = Friendship.where(user_request_receiver_id: 3, accept: true)
-  	@requests = Friendship.where(user_request_receiver_id: 3, accept: nil)
-  	@requests_pending = Friendship.where(user_request_sender_id: 3, accept: nil)
-  	sql = "SELECT * FROM comments_users_and_posts WHERE post_id = 3"
-  	@result = ActiveRecord::Base.connection.execute(sql)
-  	@result.to_a
+  	
   end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,5 +13,14 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+  	@posts_me = Post.where(user_sender_id: params[:id], user_receiver_id: nil)
+  	@posts_others = Post.where(user_receiver_id: params[:id])
+  	@friends = Friendship.where(user_request_sender_id: params[:id], accept: true)
+  	@friends2 = Friendship.where(user_request_receiver_id: params[:id], accept: true)
+  	@requests = Friendship.where(user_request_receiver_id: params[:id], accept: nil)
+  	@requests_pending = Friendship.where(user_request_sender_id: params[:id], accept: nil)
+  	sql = "SELECT * FROM comments_users_and_posts WHERE post_id = #{params[:id]}"
+  	@result = ActiveRecord::Base.connection.execute(sql)
+  	@result.to_a
   end
 end
